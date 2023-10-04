8eb26969ea5d2df172c80d4609ef53319ee945dd
monubukky <65822986+monubucky@users.noreply.github.com>
2023-10-04 10:38:24 +0530

    Update maxnum.py
    This is simple change to avoid multiple code lines of for loop.

--- maxnum.py
+++ maxnum.py
@@ -3,8 +3,5 @@ n=int(input("Enter num"))
 for i in range(0,n):
   i=int(input())
   numbers.append(i)
-max_value = None
-for num in numbers:
-    if (max_value is None or num > max_value):
-        max_value = num
+max_value = max(numbers)
 print('Maximum value:', max_value)
